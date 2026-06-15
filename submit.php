9b67f8509eb644c8bc9514f9c8cc2a2d8c6b8f27
Hansadi Kumanayake <hansadi.kumanayake@gmail.com>
2026-06-15 15:05:21 +0530

    Upload the updated version of submit.php

--- submit.php
+++ submit.php
@@ -1,17 +1,25 @@
 <?php
-//connect the php to database
+//connect the database
 include 'db.php';
  
 
 //getting form data
-
 $name=$_POST["name"];
 $tel=$_POST["tel"];
 $disaster=$_POST["disaster"];
 $geotag=$_POST["geotag"];
-//save the uploaded photos inside the upload folder
 
+//save the uploaded photos inside the upload folder
 $photo_folder="uploads/";
 
+//function to upload photo
+function uploadPhoto($photoName,$photo_folder){
+//to check whether photo is uploaded correctly
+// 1 check whether file input exists
+// 2 check whether uplaod had no error
+    if(isset($_FILES[$photoName])&& $_FILES[$photoName]['error']==0){
+        
+    }
+}
 
 ?>
