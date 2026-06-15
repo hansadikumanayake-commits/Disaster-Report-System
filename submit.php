8992c92408d38a0b042b9cf8a20879ff10b98935
Hansadi Kumanayake <hansadi.kumanayake@gmail.com>
2026-06-15 17:00:39 +0530

    Upload the update the submission file

--- submit.php
+++ submit.php
@@ -18,8 +18,20 @@ function uploadPhoto($photoName,$photo_folder){
 // 1 check whether file input exists
 // 2 check whether uplaod had no error
     if(isset($_FILES[$photoName])&& $_FILES[$photoName]['error']==0){
+        // creating the filename with the uploading time
+        $file_name=time()."_".basename($_FILES[$photoName]['name']);
         
+        //create full path
+        $target_file=$photo_folder.$file_name;
+
+        //move upload photos to uploads folder
+        //move an uploaded file from php's temp folder to our own folder 
+        if(move_uploaded_file($_FILES[$photoName]['tmp_name'],$target_file)){
+            return $target_file; // this will return to the final place where we want to save the photo
+        }
+
     }
+    return "";
 }
 
 ?>
