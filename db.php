c8bf1d4a3b9d148620e30e59d600512a78905245
Hansadi Kumanayake <hansadi.kumanayake@gmail.com>
2026-06-16 11:44:37 +0530

    Upload the updated database file

--- db.php
+++ db.php
@@ -1,17 +1,28 @@
 <?php
-
+    //database server name
     $db_server="localhost";
+
+    //database username
     $db_user="root";
+
+    //database password
     $db_pass="";
+
+    //name of the database to be connected
     $db_name="disaster_db";
+
+    //variable to store database connection
     $conn="";
 
     try{
+        //create connection with mysql database
         $conn=mysqli_connect($db_server,$db_user,$db_pass,$db_name);
     }
+
     catch(mysqli_sql_exception){
         echo "could not connect <br>";
     }
+    //check if the connection is not successful
     if(!$conn){
         echo "database not connected";
     }
