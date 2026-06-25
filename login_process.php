e64f3561d4b999dae71d57cd2301f9ef5d555159
Hansadi Kumanayake <hansadi.kumanayake@gmail.com>
2026-06-25 16:18:47 +0530

    Upload the files

--- login_process.php
+++ login_process.php
@@ -56,7 +56,27 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
 </html>
 <?php
 
-    }else{
+    }else{?>
+    <!DOCTYPE HTML>
+    <html>
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width,initial-scale=1.0">
+            <title>Login Failed</title>
+            <link rel="stylesheet" href="style.css">
+        </head>
+        <body>
+            <div class="message-container error-box">
+                <h1>Login Failed</h1>
+
+                <p class=message-text>Invalid username or password</p>
+                <a href="login.php" class="message-button">Try Again</a>
+            </div>
+
+        </body>
+    </html>
+        
+    <?php }
 }else{
     header('Location:login.php');
 }
