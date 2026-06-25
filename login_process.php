05c04e9323fcbfe3026fa74d4389223db1d2facd
Hansadi Kumanayake <hansadi.kumanayake@gmail.com>
2026-06-25 12:04:02 +0530

    Upload the updated username and password fetching part

--- login_process.php
+++ login_process.php
@@ -1,4 +1,18 @@
 <?php
 session_start();
 include 'db.php';
+
+if($_SERVER['REQUEST_METHOD']=="POST"){
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+
+    //check usernameand password from theuser table
+
+    $sql="SELECT * FROM users WHERE username='$username' AND password=$password";
+    $result=mysqli_query($conn,$sql);
+
+    if(mysqli_num_rows($result)==1){
+        $user=mysqli_fetch_assoc($result);
+    }
+}
 ?>
