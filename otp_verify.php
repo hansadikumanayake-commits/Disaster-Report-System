2310a3df29d37c28f02ca6cbfc8ee6ae20022c7a
Hansadi Kumanayake <hansadi.kumanayake@gmail.com>
2026-06-27 19:21:28 +0530

    Upload the updates

--- otp_verify.php
+++ otp_verify.php
@@ -1,4 +1,12 @@
 <?php
+//to remeber the user after usernmae and passwords are correct
 session_start();
+
+//check whether user came after login process
+//If user_id is not saved in session, send the user back to login.php.
+if(!isset($_SESSION["user_id"])){
+    header("Location:login.php");
+    exit();
+}
 
 ?>
