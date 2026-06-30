da363b0cdb1f86a79a4fe5228327546b32db84b9
Hansadi Kumanayake <hansadi.kumanayake@gmail.com>
2026-06-30 09:58:36 +0530

    Upload the updated stylings

--- otp_process.php
+++ otp_process.php
@@ -2,11 +2,14 @@
 session_start();
 include 'db.php';
 
-//checking whether the user_idis available in the session
+//checking whether the user_id is available in the session(user came after login)
 if(!isset($_SESSION["user_id"])){
     header("Location:login.php");
     exit();
 }
+
+//get the otp entered by the user
+$entered_otp=$_POST['']
 
 
 ?>
