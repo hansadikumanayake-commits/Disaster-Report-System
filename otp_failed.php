7dd1463562c4a31caaa4261df76c7923dd03393c
Hansadi Kumanayake <hansadi.kumanayake@gmail.com>
2026-06-30 09:46:07 +0530

    Add a otp failed message file

---
+++ otp_failed.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <title>OTP Verification Failed</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="message-container error-box">
+            <h1>OTP Verification Failed</h1>
+            <p class="message-text">The OTP you entered is incorrect</p>
+            <a href="otp_verify.php" class="message-button">Try Again</a>
+            
+            <a href="login.php" class="back-link">Back to login</a>
+
+        </div>
+    </body>
+</html>
