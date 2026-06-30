f4c6c422d753767a40220b1b453c9958ceb797ef
Hansadi Kumanayake <hansadi.kumanayake@gmail.com>
2026-06-30 10:37:13 +0530

    Add the updated styling

--- otp_failed.php
+++ otp_failed.php
@@ -11,7 +11,7 @@
             <h1>OTP Verification Failed</h1>
             <p class="message-text">The OTP you entered is incorrect</p>
             <a href="otp_verify.php" class="message-button">Try Again</a>
-            <br><br>
+            <br><br><br>
             <a href="login.php" class="back-link">Back to login</a>
 
         </div>
